c33896365c2dadab7afaba8d3cf167beda6128a6
Vokker <turboruslan.com@gmail.com>
2024-09-11 00:18:39 +0500

    Coin added

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -2,4 +2,10 @@ extends Area2D
 
 
 func _on_body_entered(body):
-	print("Coin collected!")
+	if body.is_in_group("Player"):
+		$AnimationPlayer.play("collected")
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "collected":
+		queue_free()
