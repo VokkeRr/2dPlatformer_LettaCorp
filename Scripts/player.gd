ef1423fc344ab5d6af9146f3cfdad797d8ceb446
Vokker <turboruslan.com@gmail.com>
2024-09-10 19:17:54 +0500

    Added Animations and Tilemap

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,25 +1,30 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -575.0
 
 
 func _physics_process(delta):
-	# Add the gravity.
+
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		$AnimatedSprite2D.play("jump")
 
-	# Handle jump.
+
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			$AnimatedSprite2D.play("walk")
+		$AnimatedSprite2D.flip_h = direction < 0 
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
